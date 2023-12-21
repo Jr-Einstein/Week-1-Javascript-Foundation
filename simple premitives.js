829b6a413fb108b941f9dc39fdaa72014511af89
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2023-12-21 17:49:45 +0530

    Add files via upload

---
+++ simple premitives.js
@@ -0,0 +1,42 @@
+console.log("Aman");
+
+// single threaded nature of js , complier and intrepreter , why js over other languages - Theory Learnt!
+// static vs dynamic languages
+
+
+// var a=1; ( dynamic lang)
+// a=2;
+// console.log(a);
+
+
+//let,var,const
+
+
+// print full name
+//  let firstname = "Aman";
+//  let middlename = "Kumar";
+//  let lastname = "Singh";
+//  console.log("The Users Full name is " + firstname + " "+ middlename + " "+lastname)
+
+// //if-else
+
+// let firstname = "Aman";
+// let middlename = "Kumar";
+// let lastname = "Singh";
+// let education = "graduate";
+
+// if(education=="graduate"){
+//     console.log("He is Educated!")
+// }
+// else{
+//     console.log("He's not Educated Much.")
+// }
+
+
+let answer=0;
+
+for(let i=0;i<100;i++){
+    answer=answer+i;
+
+}
+console.log(answer)
