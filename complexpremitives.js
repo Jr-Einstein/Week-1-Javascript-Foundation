9174aacc04bf8c4b6f87fdc8d8de4b4481d2d396
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2023-12-22 00:38:29 +0530

    Add files via upload

---
+++ complexpremitives.js
@@ -0,0 +1,27 @@
+// //fine even numbers in  a array
+
+// const ages = [21,22,23,24,25,26,30,32,33,43,432,24,123,46,51,52,58];
+
+// for(let i=0;i<ages.length;i++){
+//     if(ages[i] % 2 == 0){
+//         console.log(ages[i]);
+// }
+// }
+
+
+// const personarray=["aman","ankit","anjali"];
+// const gender =["male" , "male","female"];
+
+// const numofusers=personarray.length;
+
+// for(let i=0;i<numofusers;i++){
+//     if(gender[i]=="male"){
+//         console.log(personarray[i])
+//     }
+// }
+
+
+function findsum(a,b){
+    return a+b;
+}
+
