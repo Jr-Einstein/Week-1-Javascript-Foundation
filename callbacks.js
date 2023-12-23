9e0db3c208238f2ac3d209349f82d405eb160851
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2023-12-23 06:27:42 +0530

    Add files via upload

---
+++ callbacks.js
@@ -0,0 +1,30 @@
+// function calculateArithmetic(a,b,type){
+//     if (type=="sum"){
+//         return a+b;
+//     }
+//     if (type=="minus"){
+//         return a-b;
+//     }
+// }
+// const value= calculateArithmetic(1,2,"minus");
+// console.log(value);
+
+
+// function calculateArithmetic(a,b,arithemeticFinalfunction){
+//     const ans=arithemeticFinalfunction(a,b)
+//     return ans;
+// }
+
+// function sum(a,b){
+//     sum=a+b;
+//     return sum;
+// }
+
+// const value = calculateArithmetic(1,2,sum);
+//     console.log(value);
+
+function greet(){
+    console.log("Hello Aman");
+}
+
+setTimeout(greet,3*1000);
