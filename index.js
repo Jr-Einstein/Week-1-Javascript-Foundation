c72814f118aec379a046b72c400e40ed886b460b
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-01-04 00:03:15 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const fs= require("fs");
+const fs = require("fs");
 //filesystem module
 
-fs.readFile("a.txt","utf-8", function(err ,data){
-    console.log(data);
-})
+fs.readFile("a.txt", "utf-8", function (err, data) {
+  console.log(data);
+});
 
+console.log("Hi Aman");
+let a = 0;
+for (let i = 1; i < 100; i++) {
+  a++;
+}
+console.log("Hey Aman Kumar Singh");
